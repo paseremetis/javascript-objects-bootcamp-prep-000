c424f0353cb917fa70bc1dfaa9c4a9155502268b
Peter Seremetis <pswriter5348@gmail.com>
2018-01-25 23:59:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,1 @@
-function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
-  obj [key] = value
-  return obj
-}
-
-const recipe { eggs: 3 }
-
-destructivelyUpdateObjectWithKeyAndValue(recipe, 'flour', '3 cups')
-return { eggs: 3, flour: '3 cups' }
+var playlist = 
