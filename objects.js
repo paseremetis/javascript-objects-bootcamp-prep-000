5d2a31ad1a64e60e383d62cf9b3a1ce8236d8a3d
Peter Seremetis <pswriter5348@gmail.com>
2018-01-26 00:12:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,7 @@ var playlist = {
  artist: "Phil Ochs",
  songtitle: "Here's to the State of Mississippi"
 };
+function updatePlaylist()
 Object.assign({}, playlist, {artist: "Phil Ochs"})
 
 Object.assign({}, playlist, {songtitle: "Here's to the State of Mississippi"})
